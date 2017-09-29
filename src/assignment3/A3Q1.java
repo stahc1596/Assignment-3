5a8404285ac86dc5aa539947d40e44fdfb222a21
stahc1596 <stahc1596@HRH-IC0027341.wrdsb.ca>
2017-09-29 10:14:17 -0400

    Question will always swap even if the number is in the right position, need an if statement on the swap.

--- src/assignment3/A3Q1.java
+++ src/assignment3/A3Q1.java
@@ -10,10 +10,39 @@ package assignment3;
  */
 public class A3Q1 {
 
-    /**
-     * @param args the command line arguments
-     */
+    public void swap(int[] array, int p1, int p2){
+        int temp = array[p1];
+        array[p1] = array[p2];
+        array[p2] = temp;
+    }
+    public void selectionSort(int[] array){
+        int low = 0;
+        //Keep track of which position we are sorting
+        for(int position = 0; position < array.length; position++){
+            //Go through the rest looking for a smaller number
+            for(int i = position + 1; i < array.length; i++){
+                if(array[i] < array[low]){
+                    low = i;
+                }
+            }
+                swap(array, low, position);
+        }
+    }
     public static void main(String[] args) {
-        // TODO code application logic here
+        A3Q1 test = new A3Q1();
+        int[] numbers = {5, 7, 3, 8, 2, 1, 9, 4, 6};
+        //Before
+        System.out.println("BEFORE: ");
+        for(int i = 0; i < numbers.length; i++){
+            System.out.println(numbers[i]);
+        }
+        //Type of sort
+        test.selectionSort(numbers);
+        //After
+        System.out.println("AFTER: ");
+        for(int i = 0; i < numbers.length; i++){
+            System.out.println(numbers[i]);
+        }
+        
     }
 }
