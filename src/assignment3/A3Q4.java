81f65862c9f0f051a04a7a1bdee4786d4530e361
stahc1596 <stahc1596@HRH-IC0027341.wrdsb.ca>
2017-10-05 09:57:10 -0400

    Fin

--- src/assignment3/A3Q4.java
+++ src/assignment3/A3Q4.java
@@ -11,27 +11,29 @@ package assignment3;
 public class A3Q4 {
 
     public void countingSort(int[] array){
-        int[] count = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        int[] count = new int[101];
         for(int i = 0; i < array.length; i++){
             count[array[i]]++;
         }
-        for(int i = 0; i < count.length; i++){
-            for(int n = 0; n < count[i]; n++){
-                System.out.println(i);
-            }
+        for(int i = 0; i < array.length; i++){
+            array[i] = count[i];
         }
     }
     public static void main(String[] args) {
         A3Q4 test = new A3Q4();
-        int[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] numbers = new int[101];
+        for(int i = 0; i < numbers.length; i++){
+            numbers[i] = (int)(Math.random()*101);
+        }
         for(int i = 0; i < numbers.length; i++){
             System.out.println(numbers[i]);
         }
         System.out.println("-----------------------------");
         test.countingSort(numbers);
-        
-//        for(int i = 0; i < numbers.length;i++){
-//            System.out.println(numbers[i]);
-//        }
+        for(int i = 0; i < numbers.length; i++){
+            for(int n = 0; n < numbers[i]; n++){
+            System.out.println(i);
+            }
+        }
     }
 }
